6e06eab1915ee19f5b1126362b0d5f3ce63830c0
Mac <mac@192.168.0.192>
2026-03-24 21:10:03 +0530

    Fix Content Security Policy to allow sidebar clicks

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(
     contentSecurityPolicy: {
       directives: {
         "default-src": ["'self'"],
-        "script-src-attr": ["'unsafe-inline'"]
+        "script-src-attr": ["'unsafe-inline'"],
         "script-src": ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net"],
         "style-src": ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://cdnjs.cloudflare.com", "https://fonts.googleapis.com"],
         "font-src": ["'self'", "https://fonts.gstatic.com", "https://cdnjs.cloudflare.com"],
